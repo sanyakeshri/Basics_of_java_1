48611e922b80d462bad9d20f649b2fe71b725fa2
Sanya keshri <keshrisanya621@gmail.com>
2026-06-04 07:00:42 +0530

    updating

---
+++ A.java
@@ -0,0 +1,16 @@
+//Lec 14(Arithmetic Operators 1):
+class A
+{
+    public static void main(String[]args)
+    {
+        int num1 = 7;
+        int num2 = 5;
+
+        int result = num1 + num2;
+        // int result = num1 - num2;
+        // int result = num1 * num2;
+        // int result = num1 / num2;  //get Quotient
+        // int result = num1 % num2; //give reminder
+        System.out.println(result);
+    }
+}
