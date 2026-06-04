ab3fb5bab52a3b4bba6e6978acc2af0a036f3737
Sanya keshri <keshrisanya621@gmail.com>
2026-06-05 05:01:27 +0530

    Operators

---
+++ C.java
@@ -0,0 +1,19 @@
+//Lec 15(Relational Operators):
+class A
+{
+    public static void main(String[]args)
+    {
+        int x = 6;
+        int y = 5;
+
+        // boolean result = x < y;      // Y Greater
+        // boolean result = x > y;      // Y Lesser
+        // boolean result = x >= y;     //Greater than or equal_too
+        // boolean result = x <= y;     //Less than or equal_too
+        // boolean result = x == y;         // equal
+        // boolean result = x != y;        //not-equal
+
+
+        System.out.println(result);
+    }
+}
